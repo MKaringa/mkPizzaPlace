88f6b2bcdffcc7a5074ae8b09c0d75c47a6d4ca0
MKaringa <mungaikaringa1@gmail.com>
2021-04-27 10:51:47 +0300

    Get the inputted size

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// getSize function
+ 
+function getSize (size) {
+    if (size === 0) {
+        return "Pizza Pie"
+   }else if (size === 1) {
+        return "Small"
+    }else if (size === 2) {
+        return "Medium"
+    }else if (size === 3) {
+     return "Large"
+   }
+ };
