db58a9ad48c668b5c4c2d4aa687eb53440024b48
MKaringa <mungaikaringa1@gmail.com>
2021-04-27 10:55:36 +0300

    Get the order

--- js/script.js
+++ js/script.js
@@ -12,3 +12,19 @@ function getSize (size) {
      return "Large"
    }
  };
+
+ //  Order function
+
+$(document).ready(function(){
+    $('#submit').click(function(event){
+        event.preventDefault();
+        var type = $('#type').val();
+        var size = parseInt($("input[name='size']:checked").val());
+        var crust = parseInt($("input[name='crust']:checked").val());
+        var amount = parseInt($("#number").val());
+        var price=[600, 800, 1000, 1200];
+        var sumToppings = 0;
+        $("input[name='topping']:checked").each(function(){
+        sumToppings+= parseInt($(this).val());
+    })
+});
